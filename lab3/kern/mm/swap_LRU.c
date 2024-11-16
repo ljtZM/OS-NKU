c9246214003b56cbc0373e9b7c7dd500b5dd98ce
ljtZM <837767633@qq.com>
2024-11-16 19:41:35 +0800

    合并与更新

--- lab3/kern/mm/swap_LRU.c
+++ lab3/kern/mm/swap_LRU.c
@@ -7,11 +7,9 @@
 #include <list.h>
 #include <sbi.h>
 
+static list_entry_t pra_list_head;//一个链表头节点，用于维护一组页面
 
-static list_entry_t pra_list_head;
-
-static int
-_lru_init_mm(struct mm_struct *mm)
+static int _lru_init_mm(struct mm_struct *mm)//初始化一个虚拟内存管理结构
 {     
      list_init(&pra_list_head);
      mm->sm_priv = &pra_list_head;
@@ -19,28 +17,28 @@ _lru_init_mm(struct mm_struct *mm)
      return 0;
 }
 
-static int
-_lru_map_swappable(struct mm_struct *mm, uintptr_t addr, struct Page *page, int swap_in)
+//标记页面可使用，将页面添加到链表中
+static int _lru_map_swappable(struct mm_struct *mm, uintptr_t addr, struct Page *page, int swap_in)
 {
-    list_entry_t *head=(list_entry_t*) mm->sm_priv;
-    list_entry_t *entry=&(page->pra_page_link);
+    list_entry_t *head=(list_entry_t*) mm->sm_priv;//进程的私有链表头
+    list_entry_t *entry=&(page->pra_page_link);//指向页面的链表条目
  
-    assert(entry != NULL && head != NULL);
+    assert(entry != NULL && head != NULL);//确保指针有效
     //record the page access situlation
 
-    list_add(head, entry);
+    list_add(head, entry);//将页面的链表条目添加到链表的头部
     return 0;
 }
 
-static int
-_lru_swap_out_victim(struct mm_struct *mm, struct Page ** ptr_page, int in_tick)
+//选择一个页面换出，将其从链表中删除并返回
+static int _lru_swap_out_victim(struct mm_struct *mm, struct Page ** ptr_page, int in_tick)
 {
      list_entry_t *head=(list_entry_t*) mm->sm_priv;
          assert(head != NULL);
-     assert(in_tick==0);
+     assert(in_tick==0);//保交换操作发生在正确的时机
      /* Select the victim */
 
-    list_entry_t* entry = list_prev(head);
+    list_entry_t* entry = list_prev(head);//获取链表中倒数第一个元素
     if (entry != head) {
         list_del(entry);
         *ptr_page = le2page(entry, pra_page_link);
@@ -50,8 +48,8 @@ _lru_swap_out_victim(struct mm_struct *mm, struct Page ** ptr_page, int in_tick)
     return 0;
 }
 
-static int
-_lru_check_swap(void) {
+//模拟页面的访问并触发页面交换
+static int _lru_check_swap(void) {
     //pte_t* ptep = get_pte()
     // 页面状态：d1 c1 b1 a1
     // 假设发生一次时钟中断，导致lru
@@ -84,21 +82,20 @@ _lru_check_swap(void) {
     return 0;
 }
 
-
-static int
-_lru_init(void)
+//初始化 LRU 管理器
+static int _lru_init(void)
 {
     return 0;
 }
 
-static int
-_lru_set_unswappable(struct mm_struct *mm, uintptr_t addr)
+//标记某个地址的页面为不可交换
+static int _lru_set_unswappable(struct mm_struct *mm, uintptr_t addr)
 {
     return 0;
 }
 
-static int
-_lru_tick_event(struct mm_struct *mm)
+//处理时钟中断，遍历链表并更新页面的访问位
+static int _lru_tick_event(struct mm_struct *mm)
 { 
     list_entry_t* head = (list_entry_t*)mm->sm_priv;
     list_entry_t* cur = head;
@@ -115,12 +112,11 @@ _lru_tick_event(struct mm_struct *mm)
             list_add(head, cur);
             cur = temp;
         }
-        // cprintf("here in lru_tick_event\n");
     }
     return 0;
 }
 
-
+//定义一个 swap_manager 结构体，包含 LRU 算法相关的操作函数
 struct swap_manager swap_manager_lru =
 {
      .name            = "lru swap manager",
